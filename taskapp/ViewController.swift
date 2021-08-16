3ab7bbcad80985e8b1016ca5e6c4b4bb430fc29c
naoki.nishio <naooo8021@icloud.com>
2021-08-16 10:00:04 +0900

    Initial Commit

---
+++ taskapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  taskapp
+//
+//  Created by 中西八洋 on 2021/08/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
